622df56559a48c9ffdd44758306ce831a6e670fc
Janaki <janakiu3@gmail.com>
2020-06-25 13:07:32 +1000

    Specflow

---
+++ Mars/Mars/Pages/AutoItX3.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mars.Pages
+{
+    internal class AutoItX3
+    {
+        public AutoItX3()
+        {
+        }
+
+        internal void WinActivate(string v)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal void Send(string imagePath)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
---
+++ Mars/Mars/Utils/Start.cs
@@ -0,0 +1,66 @@
+﻿using Mars.Pages;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mars.Utils
+{
+    public class Start
+    {
+        [TestFixture]
+        [Category("Sprint1")]
+        class User : Global.Base
+        {
+
+            [Test]
+            [System.Obsolete]
+            public void ShareSkillTest()
+            {
+                ShareSkill shareSkill = new ShareSkill();
+                shareSkill.EnterShareSkill();
+
+                bool Result = shareSkill.ValidateShareSkill(Global.GlobalDefinitions.driver);
+                Assert.IsTrue(Result);
+            }
+
+            [Test]
+            [System.Obsolete]
+            public void EditListingTest()
+            {
+                ManageListings manageListings = new ManageListings();
+                manageListings.EditListings();
+
+                bool EditResult = manageListings.ValidateEdit(Global.GlobalDefinitions.driver);
+                Assert.IsTrue(EditResult);
+            }
+
+            [Test]
+            [System.Obsolete]
+            public void DeleteListingTest()
+            {
+                ManageListings manageListings = new ManageListings();
+                manageListings.DeleteListings();
+
+                bool DelResult = manageListings.ValidateDelete(Global.GlobalDefinitions.driver);
+                Assert.IsTrue(DelResult);
+            }
+
+            [Test]
+            [System.Obsolete]
+            public void ChangePassword()
+            {
+                Profile profile = new Profile();
+                profile.ChangePassword();
+
+                bool PwdChgResult = profile.ValidateChangePassword(Global.GlobalDefinitions.driver);
+                Assert.IsTrue(PwdChgResult);
+            }
+
+
+        }
+
+    }
+}
